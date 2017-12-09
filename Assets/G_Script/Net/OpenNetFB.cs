7f4794e8f28e019bcfd76e05f1969d01ce92fa1b
s890218 <wl00882105@gmail.com>
2017-12-09 16:16:16 +0800

    初始化

---
+++ Assets/G_Script/Net/OpenNetFB.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenNetFB : MonoBehaviour {
+
+	private string m_SelfFb = "https://www.facebook.com/";
+
+
+	void OnClickFB () {
+		Application.OpenURL(m_SelfFb);
+	}
+	
+}
